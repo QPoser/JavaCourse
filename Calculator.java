799701ba08f5d062199125b84b1733c444c2f8af
AndreykaMe <andystory@mail.ru>
2017-04-28 19:03:56 +0700

    Base Calculator

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,21 @@ public class Calculator {
     
     private int result = 0;
     
-    public void add(int x, int y)
+    public void plus(int x, int y)
     {
         this.result = x + y;
+    }
+    
+    public void minus(int x, int y) {
+        this.result = x - y;
+    }
+    
+    public void multiply(int x, int y) {
+        this.result = x * y;
+    }
+    
+    public void dividing(int x, int y) {
+        this.result = x / y;
     }
     
     public int getResult() {
--- Runner.java
+++ Runner.java
@@ -12,7 +12,29 @@ public class Runner {
                 String first = reader.next();
                 System.out.println("Enter 2st arg:");
                 String second = reader.next();
-                calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                System.out.println("Choose opertation: plus|minus|multiply|dividing");
+                String type = reader.next();
+                switch (type) {
+                    case "plus":
+                        calc.plus(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                        
+                    case "minus":
+                        calc.minus(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                        
+                    case "multiply":
+                        calc.multiply(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                        
+                    case "dividing":
+                        calc.dividing(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                        
+                    default: 
+                        System.out.println("Неправильный тип");
+                        break;
+                }
                 System.out.println("Result: " + calc.getResult());
                 calc.cleanResult();
                 System.out.println("Exit : yes|no ");
