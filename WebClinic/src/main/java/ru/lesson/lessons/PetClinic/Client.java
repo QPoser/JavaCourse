c6acfb8b6b88fd1edb22c38b6dc3f48e94e2cb0d
andrey <andystory@mail.ru>
2017-06-02 12:21:39 +0700

    Ubuntu update test

--- WebClinic/src/main/java/ru/lesson/lessons/PetClinic/Client.java
+++ WebClinic/src/main/java/ru/lesson/lessons/PetClinic/Client.java
@@ -37,7 +37,7 @@ public class Client {
                 this.pet = null;
             }
             else {
-                System.out.println(name + ": i have a pet!");
+                System.out.println(name + ": i have a pet!!");
             }
         }
     }
